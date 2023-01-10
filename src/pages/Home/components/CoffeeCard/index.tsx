125f92722e82c0e255a2a042d4685e8801e2fe72
Zac Rodrigues <rodrigues.contano@gmail.com>
2023-01-10 20:34:04 -0300

    Página home estruturas e estilos

---
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -0,0 +1,53 @@
+import { QuantityInput } from "../../../../components/QuantityInput";
+import { RegularText, TitleText } from "../../../../components/Typography";
+import { AddCartWrapper, CardFooter, CoffeeCardContainer, Description, Name, Tags } from "./styles";
+import { ShoppingCart } from 'phosphor-react'
+import { formatMoney } from "../../../../utils/formatMoney";
+
+export interface Coffee {
+  id: number
+  tags: string[]
+  name: string
+  description: string
+  photo: string
+  price: number
+}
+
+interface CoffeeCardProps {
+  coffee: Coffee
+}
+
+export function CoffeeCard({ coffee }: CoffeeCardProps) {
+  const formattedPrice = formatMoney(coffee.price)
+
+  return (
+    <CoffeeCardContainer>
+      <img src={`/coffees/${coffee.photo}`} alt="" />
+
+      <Tags>
+        {coffee.tags.map(tag => (
+          <span key={`${coffee.id}${tag}`}>{tag}</span>
+        ))}
+      </Tags>
+
+      <Name>{coffee.name}</Name>
+
+      <Description>{coffee.description}</Description>
+
+      <CardFooter>
+        <div>
+          <RegularText size="s">R$</RegularText>
+          <TitleText size="m" color="text" as="strong">{formattedPrice}</TitleText>
+        </div>
+
+        <AddCartWrapper>
+          <QuantityInput />
+
+          <button>
+            <ShoppingCart weight="fill" size={22} />
+          </button>
+        </AddCartWrapper>
+      </CardFooter>
+    </CoffeeCardContainer>
+  )
+}
