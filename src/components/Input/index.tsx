fe4ae3a68ddb8a32d9bce30ecc50a43d27994e03
Zac Rodrigues <rodrigues.contano@gmail.com>
2023-01-13 21:33:06 -0300

    Finalizando desafio

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,31 @@
-import { InputHTMLAttributes } from 'react'
-import { InputStyleContainer } from './styles'
+import { forwardRef, InputHTMLAttributes } from 'react'
+import { RegularText } from '../Typography'
+import {
+  InputStyleContainer,
+  InputWrapper,
+  InputStyled,
+  RightText,
+} from './styles'
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>
-
-export function Input({ ...props }: InputProps) {
-  return <InputStyleContainer {...props} />
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string
+  rightText?: string
 }
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, className, rightText, ...props }, ref) => {
+    return (
+      <InputWrapper className={className}>
+        <InputStyleContainer hasError={!!error}>
+          <InputStyled {...props} ref={ref} />
+          {rightText && <RightText>{rightText}</RightText>}
+        </InputStyleContainer>
+        {error && <RegularText size="s">{error}</RegularText>}
+      </InputWrapper>
+    )
+  },
+)
+
+Input.displayName = 'Input'
+
+export default Input
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -16,6 +16,7 @@ interface CartContextType {
     type: 'increase' | 'decrease',
   ) => void
   removeCartItem: (cartItemId: number) => void
+  cleanCart: () => void
 }
 
 interface CartContextProviderProps {
@@ -90,6 +91,10 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     setCartItems(newCart)
   }
 
+  function cleanCart() {
+    setCartItems([])
+  }
+
   useEffect(() => {
     localStorage.setItem(COFFEE_ITEMS_STORAGE_KEY, JSON.stringify(cartItems))
   }, [cartItems])
@@ -103,6 +108,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
         addCoffeeToCart,
         changeCartItemQuantity,
         removeCartItem,
+        cleanCart,
       }}
     >
       {children}
